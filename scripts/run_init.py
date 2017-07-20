4b058214da7afba4caddd80ec62469f3a1a0d765
Hecmay <xiang.elec@gmail.com>
2017-07-21 03:59:32 +0800

    Update run_init.py

--- scripts/run_init.py
+++ scripts/run_init.py
@@ -10,7 +10,7 @@ proc_num = 1
 datpath = sys.path[0]
 srcFile = datpath+"/run_DATuner.py"
 
-rep_cmd = "sed -e \"s:TOOL_PATH:"+tpath+":g\" -e \"s:DESIGN_NAME:"+design+":g\" -e \"s:WROKSPACE:"+wrksp+":g\" -e \"s:DATuner_PATH:"+datpath+":g\" -e \"s:PROC_NUM:"+str(proc_num)+":g\" "+srcFile+" > "+datpath+"/datuner"+"_new.py"
+rep_cmd = "sed -e \"s:TOOL_PATH:"+tpath+":g\" -e \"s:DESIGN_NAME:"+design+":g\" -e \"s:WORKSPACE:"+wrksp+":g\" -e \"s:DATuner_PATH:"+datpath+":g\" -e \"s:PROC_NUM:"+str(proc_num)+":g\" "+srcFile+" > "+datpath+"/datuner"+"_new.py"
 os.system(rep_cmd)
 
 run_cmd = "python "+ pwd + "/releases/Linux_x86_64/scripts/datuner_new.py -f vtr -t 20 2 0 0.2197 -p 2 -b 1000"
