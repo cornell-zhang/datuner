53089ee538f06624fcc5375aa56220e2f591c512
Hecmay <xiang.elec@gmail.com>
2017-07-21 03:10:15 +0800

    Update run_init.py

--- scripts/run_init.py
+++ scripts/run_init.py
@@ -1,9 +1,10 @@
 import os
 import sys
 
-tpath = "/work/zhang/users/sx233/datuner/build/pkgs/vtr/vtr_release/vtr_flow"
+pwd =os.getcwd()
+tpath = pwd + "/build/pkgs/vtr/vtr_release/vtr_flow"
 design = "diffeq1"
-wrksp = "/work/zhang/users/sx233/datuner/test"
+wrksp = pwd + "/test"
 proc_num = 1
 
 datpath = sys.path[0]
@@ -12,5 +13,7 @@ srcFile = datpath+"/run_DATuner.py"
 rep_cmd = "sed -e \"s:TOOL_PATH:"+tpath+":g\" -e \"s:DESIGN_NAME:"+design+":g\" -e \"s:WROKSPACE:"+wrksp+":g\" -e \"s:DATuner_PATH:"+datpath+":g\" -e \"s:PROC_NUM:"+str(proc_num)+":g\" "+srcFile+" > "+datpath+"/datuner"+"_new.py"
 os.system(rep_cmd)
 
+run_cmd = "python "+ pwd + "/releases/Linux_x86_64/scripts/datuner_new.py -f vtr -t 20 2 0 0.2197 -p 2 -b 1000"
 print "call: python ./datuner_new.py -f vtr -t 20 2 0 0.2197 -p 2 -b 1000"  
-os.system("python ./datuner_new.py -f vtr -t 20 2 0 0.2197 -p 2 -b 1000")
+os.system(run_cmd)
+
