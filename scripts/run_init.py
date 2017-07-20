6d17ac583ee4b4c1700317ad5f764d1871aef654
Hecmay <xiang.elec@gmail.com>
2017-07-21 01:13:28 +0800

    Update run_init.py

--- scripts/run_init.py
+++ scripts/run_init.py
@@ -1,8 +1,3 @@
-#-----------------------------------------------
-# initialize the param of tool/datuner/workspace 
-# this info is supposed to be assigned by the user
-#-----------------------------------------------
-
 import os
 import sys
 
@@ -14,11 +9,8 @@ proc_num = 1
 datpath = sys.path[0]
 srcFile = datpath+"/run_DATuner.py"
 
-#change the NMAE_HOLDER in run_DATuner.py
-
 rep_cmd = "sed -e \"s:TOOL_PATH:"+tpath+":g\" -e \"s:DESIGN_NAME:"+design+":g\" -e \"s:WROKSPACE:"+wrksp+":g\" -e \"s:DATuner_PATH:"+datpath+":g\" -e \"s:PROC_NUM:"+str(proc_num)+":g\" "+srcFile+" > "+datpath+"/datuner"+"_new.py"
 os.system(rep_cmd)
 
-# run the datuner_new.py with the assigned paramter
-print "call: python ./datuner_new.py --tool vtr --timeout 20 2 0 0.1 --para-factor 2 --max-run 1000"  
-os.system("python ./datuner_new.py --tool vtr --timeout 20 2 0 0.2197 --para-factor 2 --max-run 1000")
+print "call: python ./datuner_new.py -f vtr -t 20 2 0 0.2197 -p 2 -b 1000"  
+os.system("python ./datuner_new.py -f vtr -t 20 2 0 0.2197 -p 2 -b 1000")
