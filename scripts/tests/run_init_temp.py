6f4ae6a061d003dacdd69dca61ef2dd34b4019cc
Hecmay <xiang.elec@gmail.com>
2017-07-21 09:21:14 +0800

    Update run_init_temp.py

--- scripts/tests/run_init_temp.py
+++ scripts/tests/run_init_temp.py
@@ -13,6 +13,6 @@ srcFile = datpath+"/run_DATuner.py"
 rep_cmd = "sed -e \"s:TOOL_PATH:"+tpath+":g\" -e \"s:DESIGN_NAME:"+design+":g\" -e \"s:WORKSPACE:"+wrksp+":g\" -e \"s:DATuner_PATH:"+datpath+":g\" -e \"s:PROC_NUM:"+str(proc_num)+":g\" "+srcFile+" > "+datpath+"/datuner"+"_new.py"
 os.system(rep_cmd)
 
-run_cmd = "python "+ pwd + "/releases/Linux_x86_64/scripts/datuner_new.py -f vtr -t 20 2 0 0.2197 -p 2 -b 1000"
-print "call: python ./datuner_new.py -f vtr -t 20 2 0 0.2197 -p 2 -b 1000"  
+run_cmd = "python "+ pwd + "/releases/Linux_x86_64/scripts/datuner_new.py -f vtr -t 20 2 0 0.2197 -p 2 -b 1"
+print "call: python ./datuner_new.py -f vtr -t 20 2 0 0.2197 -p 2 -b 1"  
 os.system(run_cmd)
