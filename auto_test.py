cf697daab2d903a24f490bb3f7b047475b069552
Hecmay <xiang.elec@gmail.com>
2017-07-21 22:53:40 +0800

    Update auto_test.py

--- auto_test.py
+++ auto_test.py
@@ -9,8 +9,8 @@ s = platform.system()
 a = platform.machine()
 
 script_folder = pwd + '/releases/' + platform.system() + '_' + platform.machine() + '/scripts'
-test_init_file = script_folder + '/tests/run_init_temp.py'
-init_out = script_folder + '/run_init.py'
+test_config_file = script_folder + '/tests/run_init_temp.py'
+config_out = script_folder + '/run_init.py'
 
 # update hostname
 f = open(script_folder + '/my_hosts', 'w')
@@ -18,8 +18,8 @@ f.write(platform.node())
 f.close()
 
 # update config file
-f1 = open(test_init_file, 'r')
-f2 = open(init_out, 'w')
+f1 = open(test_config_file, 'r')
+f2 = open(config_out, 'w')
 for line in f1:
     line = line.replace('vtr_flow_holder', vtr_path)
     line = line.replace('workspace_holder', workspace_path)
@@ -28,7 +28,7 @@ f1.close()
 f2.close()
 
 # run opentuner
-os.system('python ' + init_out)
+os.system('python ' + config_out)
 
 # check results
 
