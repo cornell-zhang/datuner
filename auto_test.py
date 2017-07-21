8e66882ef509271a12b254696da3d0a0842e2d05
Hecmay <xiang.elec@gmail.com>
2017-07-21 08:12:48 +0800

    Update auto_test.py
    update the initial workspace and tool path in the initialization program (run_init.py)

--- auto_test.py
+++ auto_test.py
@@ -9,9 +9,8 @@ s = platform.system()
 a = platform.machine()
 
 script_folder = pwd + '/releases/' + platform.system() + '_' + platform.machine() + '/scripts'
-test_config_file = script_folder + '/tests/configvtr_template.txt'
-config_out = script_folder + '/tests/configvtr.txt'
-run_script = script_folder + '/run_init.py'
+test_init_file = script_folder + '/tests/run_init_temp.py'
+init_out = script_folder + '/run_init.py'
 
 # update hostname
 f = open(script_folder + '/my_hosts', 'w')
@@ -19,8 +18,8 @@ f.write(platform.node())
 f.close()
 
 # update config file
-f1 = open(test_config_file, 'r')
-f2 = open(config_out, 'w')
+f1 = open(test_init_file, 'r')
+f2 = open(init_out, 'w')
 for line in f1:
     line = line.replace('vtr_flow_holder', vtr_path)
     line = line.replace('workspace_holder', workspace_path)
@@ -29,7 +28,7 @@ f1.close()
 f2.close()
 
 # run opentuner
-os.system('python ' + run_script)
+os.system('python ' + init_out)
 
 # check results
 
