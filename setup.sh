882a471fc2c8df3ae8baa09ad5499b2e6c52c783
Hecmay <xiang.elec@gmail.com>
2017-07-28 03:36:43 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 export PATH=$(pwd)/build/pkgs/python/install/bin:$PATH
 export LD_LIBRARY_PATH=$(pwd)/build/pkgs/python/install/lib:$LD_LIBRARY_PATH
 export PATH=$(pwd)/build/pkgs/install/bin:$PATH
+export PATH=$(pwd)/releases/Linux_x86_64/scripts:$PATH
