9cb0ac86143a7ed13ce78701b21907c473dae3c1
gl387 <gl387@cornell.edu>
2017-09-08 11:57:40 -0400

    update socket interface

--- src/host.py
+++ src/host.py
@@ -1,41 +1,24 @@
-import re
-import os
 import sys
-import optparse
-import argparse
-import getpass
 import socket
 import pickle
 
 # Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('128.84.48.152', 10000)
+server_address = ('128.253.128.53', 10000)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
+conn.bind(server_address)
 
 # Listen for incoming connections
-sock.listen(5)
-
-#-------------------------------------------------
-# parse parameters
-#-------------------------------------------------
-parser = argparse.ArgumentParser(description='main script for autotuning')
-parser.add_argument('-i', '--index', type=int, default=1, dest='index')
-parser.add_argument('-r', '--rank', type=int, default=1, dest='rank')
-args = parser.parse_args()
-
-index = str(args.index)
-rank = str(args.rank)
+conn.listen(8)
 
 while True:
   # Wait for a connection
   print >>sys.stderr, 'waiting for a connection'
-  connection, client_address = sock.accept()
+  connection, client_address = conn.accept()
   try:
-    connection.sendall('hello')
-    #connection.sendall(rank)
+    data_string = pickle.dumps(['EnumParameter', 'index', range(1000)])
+    connection.send(data_string)
   finally:
     connection.close()
-
--- src/tune_sample.py
+++ src/tune_sample.py
@@ -1,61 +1,38 @@
 import sys
-import logging
-import re
-import time
-import os
-import os.path
 import argparse
-import thread
-import adddeps
 import opentuner
-import subprocess
-import math
 from opentuner import ConfigurationManipulator
 from opentuner import EnumParameter
 from opentuner import FloatParameter
 from opentuner import MeasurementInterface
 from opentuner import Result
 import socket
+import pickle
 
 # Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('128.84.48.152', 10000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
+server_address = ('128.253.128.53', 10000)
+print >> sys.stderr, 'connecting to %s port %s' % server_address
+conn.connect(server_address)
 
 class ProgramTuner(MeasurementInterface):
-  myrank = 1
+  try:
+    data = pickle.loads(conn.recv(8096))
+    param_type, param_name, param_range = data
+  finally:
+    conn.close()
+
   def manipulator(self):
-    """
-    the msg from socket will be reiceived and then
-    passed into the manipulator
-    """
-    scope = range(1000)
     manipulator = ConfigurationManipulator()
-    try:
-      count=0
-      while count<1:
-        data = sock.recv(100)
-        if count==0:
-          index = data
-        elif count==1:
-          rank = data
-        count+=1
-    finally:
-      sock.close()
-    print data
-    manipulator.add_parameter(
-      EnumParameter('index', scope))
+    if self.param_type == 'EnumParameter':
+      manipulator.add_parameter(EnumParameter(self.param_name, self.param_range))
     return manipulator
 
   def get_qor(self):
-    filename = 'qor'+str(self.myrank)+'.txt'
-    f = open(filename,'r')   
-    lines = f.readlines()
-    qor = lines[0]
-    return float(qor)
+    f = open('qor.txt', 'r')   
+    return float(f.readlines()[0])
 
   def run(self, desired_result, input, limit):
     """
@@ -65,40 +42,33 @@ class ProgramTuner(MeasurementInterface):
     cfg = desired_result.configuration.data
     result_id = desired_result.id
 
-    #acquire configuration
-    flag = 'index'
-    type_config = ' '+str(cfg[flag])+' '
-    index = cfg[flag]    
-   
-    sample_run = "python ./sample.py " + str(self.myrank) + " " + str(index)
+    # acquire configuration
+    index = cfg[self.param_name]
+    sample_run = "python ./sample.py " + str(index)
     run_result = self.call_program(sample_run)
     assert run_result['returncode'] == 0
 
     result = self.get_qor()
-    self.dumpresult(self.myrank,cfg,result)
+    self.dumpresult(cfg, result)
     return Result(time = result)
 
   def save_final_config(self, configuration):
     """called at the end of tuning"""
     print "Optimal options written to bench_config.json:", configuration.data
-    self.manipulator().save_to_file(configuration.data,
-                                        'inline_config.json')
-  def dumpresult(self, rank, cfg, res):
+    self.manipulator().save_to_file(configuration.data, 'inline_config.json')
+
+  def dumpresult(self, cfg, res):
     """
     Compile and run a given configuration then
     return performance
     """
-    f = open('./localresult'+str(rank)+'.txt','a')
+    f = open('./localresult.txt', 'a')
     for key in cfg:
-      f.write(str(key)+" "+str(cfg[key])+" ")
+      f.write(str(key) + " " + str(cfg[key]) + " ")
     f.write(str(res))
     f.write('\n')
     f.close()
 
-
 if __name__ == '__main__':
   argparser = opentuner.default_argparser()
   ProgramTuner.main(argparser.parse_args())
-
-
-
