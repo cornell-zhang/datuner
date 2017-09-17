49f0dcca003646cca19146b352f67c48f16f5b3e
Ecenur Ustun <eu49@en-ec-zhang05.coecis.cornell.edu>
2017-09-17 12:51:19 -0400

    Fix device name

--- scripts/eda_flows/quartus/run_quartus.tcl
+++ scripts/eda_flows/quartus/run_quartus.tcl
@@ -7,7 +7,7 @@ set result_id REVISION
 load_package flow
 project_new -revision $result_id $srcdir/$topmodule -overwrite
 set_global_assignment -name TOP_LEVEL_ENTITY $topmodule
-set_global_assignment -name FAMILY "Stratix V"
-set_global_assignment -name DEVICE 5SGSD6KF40
+set_global_assignment -name FAMILY "Cyclone V"
+set_global_assignment -name DEVICE 5CGTFD9E5F35C7
 source $workdir/options.tcl
 project_close
